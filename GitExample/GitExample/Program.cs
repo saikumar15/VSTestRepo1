3df005597853d9fce4e6922a271c493d0875f9d2
saikumar15 <54535142+saikumar15@users.noreply.github.com>
2019-08-27 17:18:15 +0530

    Update Program.cs

--- GitExample/GitExample/Program.cs
+++ GitExample/GitExample/Program.cs
@@ -7,6 +7,7 @@ namespace GitExample
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Add server");
             //...............changes
         }
     }
